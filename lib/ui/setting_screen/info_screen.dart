f4685de9e7360c38d44e6d0fff60101942cd19a9
hieutran018 <0306191024@caothang.edu.vn>
2022-10-10 09:42:13 +0700

    Squashed commit of the following:
    commit 3e6ec1bd97fe548c22737ee9346147c8e37a6979
    Author: anh-huynh-1810 <0306191002@caothang.edu.vn>
    Date:   Sun Oct 9 15:37:06 2022 +0700
    
        Update info_screen.dart
    
    commit 8a98f509ec816f91d86a2d14701007f68ff01e40
    Author: anh-huynh-1810 <0306191002@caothang.edu.vn>
    Date:   Sun Oct 9 15:36:44 2022 +0700
    
        Update info_screen.dart

--- lib/ui/setting_screen/info_screen.dart
+++ lib/ui/setting_screen/info_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -5,6 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 class InfoScreen extends StatefulWidget {
   const InfoScreen({Key? key}) : super(key: key);
   static String route = "/info";
+
   @override
   State<InfoScreen> createState() => _InfoScreenState();
 }
@@ -36,10 +38,114 @@ class _InfoScreenState extends State<InfoScreen> {
         elevation: 0.0,
         backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
       ),
-      body: SingleChildScrollView(
-          child: Center(
-        child: Column(),
-      )),
+      body: ListView(
+        children: [
+          Stack(
+            children: [
+              Center(
+                child: CircleAvatar(
+                  radius: 80,
+                ),
+              ),
+              Positioned(
+                top: 120,
+                left: 230,
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.camera_alt,
+                    color: Colors.grey,
+                  ),
+                ),
+              )
+            ],
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                    ),
+                    hintText: 'ID',
+                    labelText: 'ID',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                    ),
+                    hintText: 'Email',
+                    labelText: 'Email',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                    ),
+                    hintText: 'Họ',
+                    labelText: 'Họ',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                    ),
+                    hintText: 'Tên',
+                    labelText: 'Tên',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20, left: 50, right: 50),
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                    ),
+                    hintText: 'Ngày sinh',
+                    labelText: 'Ngày sinh',
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 30, left: 40, right: 40),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.orangeAccent,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Hoàn tất",
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
